afb9d7117919e8a85d8bf30c0e97bdcf3057b39e
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2022-04-28 00:14:25 -0300

    Redirecionamento e login funcinoando

--- MercadoSunny/pages/login/login.js
+++ MercadoSunny/pages/login/login.js
@@ -3,5 +3,11 @@ function setCookie() {
     var pass = document.getElementById("password").value;
     document.cookie = "user=" + user + "; path=/";
     document.cookie = "password=" + pass + "; path=/";
-    window.location.href = "home.html";
+    if(user && pass){
+        window.location = "../home/home.html";
+        return false;
+    }
+    else{
+        alert("Usuário ou senha inválidos");
+    }
 }
