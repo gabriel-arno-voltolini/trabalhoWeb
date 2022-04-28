07f68632689deb3ab4ad0f648c55eb4c148b4018
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2022-04-27 22:55:02 -0300

    Funcao de autentication adicionada no loading da home

---
+++ MercadoSunny/pages/home/home.js
@@ -0,0 +1,24 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function setUserName() {
+    var name = getCookie("user");
+    document.getElementById('loggedUserName').textContent = "Bem-vido(a) " + name;
+}
+
+function authenticateUser() {
+    setUserName();
+}
