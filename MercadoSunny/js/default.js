9bb4999b55c9904a4d5f54b7a4ee05de12e86109
Victor do Amaral <victor.ghibli@gmail.com>
2022-04-28 19:07:07 -0300

    Mudanças no JS e tratamento do nome do usuario

--- MercadoSunny/js/default.js
+++ MercadoSunny/js/default.js
@@ -1,7 +1,35 @@
-function setLogged() {
-    var logged = false;
-
-    if (!logged) {
-        
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
+    return "";
+}
+
+function setUserName(name) {
+    document.getElementById('logged-text').textContent = name;
+}
+
+function userIsValid() {
+    var name = getCookie("user");
+    var pass = getCookie("password");
+    return name && pass;
+}
+
+function authenticateUser() {
+    var name = getCookie("user");
+    if (userIsValid()) {
+        setUserName("Bem-vindo(a) " + name);
+        return
+    }
+
+    document.getElementById("profile-item").style.display = "none";
 }
